d56ec88bda1c20b0262ce49245c920d62c24d9a4
Patryk Bialek <patryk.b@me.com>
2019-10-03 21:08:43 +0200

    add basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me.component';
+import { MyOfferComponent } from './components/my-offer.component';
+import { CooperationTermsComponent } from './components/cooperation-terms.component';
+import { ContactComponent } from './components/contact.component';
+import { FooterComponent } from './components/footer.component';
+import { HeaderComponent } from './components/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    MyOfferComponent,
+    CooperationTermsComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
