e233eb5c2de5d58d6ee5e72bebd75b5916ad4f5c
Patryk Bialek <patryk.b@me.com>
2019-10-03 23:58:26 +0200

    add contact details

--- src/app/components/contact.component.ts
+++ src/app/components/contact.component.ts
@@ -9,6 +9,26 @@ export class ContactComponent implements OnInit {
 
   title = 'Kontakt';
   description = `Kontakt`;
+
+  contact = [
+    {
+      image: {
+        fileName: 'contact_email',
+        title: 'Email'
+      },
+      title: 'Email',
+      subTitle: 'patryk.b@me.com'
+    },
+    {
+      image: {
+        fileName: 'contact_phone',
+        title: 'Phone'
+      },
+      title: 'Telefon',
+      subTitle: '+48 510 217 852'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
